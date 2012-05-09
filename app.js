0d694519a4734f6029c64b48f3b36ac42c28810d
Spike Grobstein <spikegrobstein@mac.com>
2012-05-08 22:40:32 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+$(function() {
+    var anus = $('#anus');
+    
+    throbbing_anus();
+    anal_drippage();
+});
+
+function anal_drippage() {
+    var drip = $('<img class="drip" src="https://upload.wikimedia.org/wikipedia/commons/e/ed/Red_drop.svg" />');
+    
+    $('body').append(drip);
+    
+    anus_p = $(anus).position();
+    drip_top = $(anus).height() * .68 / 2;
+    drip_left = $(anus).width() / 2 - $(drip).width() / 2;
+    
+    drip.css({
+        position: 'absolute',
+        top: drip_top - $(drip).height() * ((Math.random() - .5) * 3),
+        left: drip_left - $(drip).width() * ((Math.random() - .5) * 3),
+        color: 'red'
+    });
+    
+    drip.animate({
+        top: $(drip).position().top + 150 + 'px',
+        opacity: '0'
+    }, 1000, function() {
+        drip.remove();
+    });
+    
+    setTimeout(anal_drippage, 750 * Math.random());
+}
+
+function throbbing_anus(throbbing) {
+    var opac = throbbing ? '.2' : '.4'
+    
+    $(anus).animate({
+        opacity: opac
+    }, 1500 + 100 * Math.random(), function() {
+        throbbing_anus(!throbbing);
+    })
+}
