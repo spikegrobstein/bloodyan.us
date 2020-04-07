fa77e9a0d9394309d52ffbdc8b4d046b2c0d7401
Spike Grobstein <me@spike.cx>
2020-04-06 20:34:40 -0700

    use process environment for webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'development',
   entry: {
     index: './src/js/index.js',
     nes: './src/js/nes.js'
